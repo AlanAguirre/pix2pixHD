2abf042927766abca57ce0241498045685059361
AlanAguirre <alan_aguirre1@msn.com>
2020-05-09 02:30:06 -0300

    Add xavier init

--- models/networks.py
+++ models/networks.py
@@ -11,7 +11,7 @@ import numpy as np
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
-        m.weight.data.normal_(0.0, 0.02)
+        nn.init.xavier_uniform_(m.weight.data, gain=1.0)
     elif classname.find('BatchNorm2d') != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
